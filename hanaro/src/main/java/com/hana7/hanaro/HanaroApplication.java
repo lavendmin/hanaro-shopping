ed59ebc28e3f528fab082d301741e472c8f79fba
lavendmin <lavendmin639@gmail.com>
2025-08-07 17:06:55 +0900

    hanaro shopping project: initial setting

---
+++ hanaro/src/main/java/com/hana7/hanaro/HanaroApplication.java
@@ -0,0 +1,13 @@
+package com.hana7.hanaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanaroApplication.class, args);
+	}
+
+}
---
+++ hanaro/src/test/java/com/hana7/hanaro/HanaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana7.hanaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
